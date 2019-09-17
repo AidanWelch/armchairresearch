a5c0a17d5985723a7d4195299527e4af24470c0d
Aidan Welch <aidansw@yahoo.com>
2019-09-16 20:32:10 -0400

    added the index page and logo

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 require('openhost')(8081);
 
-
 var express = require('express');
 var app = express();
+
+var path = require('path');
+
+
+////Serve resources
+//CSS
+app.get('/common.css', (req, res) => {
+    res.sendFile(path.join(__dirname+'/common.css'))
+});
+//Favicon
+app.get('/logos/favicon.ico', (req, res) => {
+    res.sendFile(path.join(__dirname+'/logos/favicon.ico'))
+});
+////
+
+app.use(express.static("placeholders"));
+
+app.listen(80);
